ba7c9b490a88f7769d09379c7630bf71992f6337
YoungJun Park <yjpark@homex.com>
2021-08-23 11:59:41 -0400

    temp: this is the working version for testing

--- src/api_factory.py
+++ src/api_factory.py
@@ -1,4 +1,9 @@
+from src.splunk_api import *
+
 class ApiFactory:
     @classmethod
-    def get_api_factory(cls, service):
-        pass
+    def get_api_factory(self, service):
+        if service == "splunk":
+            return SplunkApi()
+        else:
+            return
