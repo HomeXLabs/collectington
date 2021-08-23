8c5fad862a69ea414febfa2a0e48c17de9741949
YoungJun Park <yjpark@homex.com>
2021-08-23 09:49:04 -0400

    feat: add excepition

---
+++ exceptions/collection_exceptions.py
@@ -0,0 +1,14 @@
+class InvalidServiceTypeException(Exception):
+    pass
+
+
+class InvalidMetricTypeException(Exception):
+    pass
+
+
+class UndefinedMetricException(Exception):
+    pass
+
+
+class UnsupportedPrometheusInstance(Exception):
+    pass
