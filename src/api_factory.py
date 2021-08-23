4494561d3d7a76519284949dddf32ff1727c9261
Nathan Boswell <nboswell@homex.com>
2021-08-23 11:59:57 +0100

    Remove unused imports

--- src/api_factory.py
+++ src/api_factory.py
@@ -1,7 +1,5 @@
 from src.splunk_api import *
 from src.callrail_api import *
-from src.deadmanswitch_api import DeadmanswitchApi
-from src.dataplatformmonitoring_api import DataplatformmonitoringApi
 
 
 class ApiFactory:
