695a205479c363315ef91e99cf50a5649481e0e1
YoungJun Park <yjpark@homex.com>
2021-08-24 16:33:23 -0400

    feat: add try except for config.py

--- src/config.py
+++ src/config.py
@@ -37,9 +37,12 @@ def read_config(config_file_path):
 def get_service_config(service_name):
     config = read_config(CONFIG_FILE_PATH)
 
-    for i in range(len(config["services"])):
-        if config["services"][i]["service_name"] == service_name:
-            return config["services"][i]
+    try:
+        for i in range(len(config["services"])):
+            if config["services"][i]["service_name"] == service_name:
+                return config["services"][i]
+    except Exception as err:
+        LOGGER.error("Failed to load service config: %s", err)
 
 
 def get_list_of_available_metrics(service_name):
@@ -48,19 +51,25 @@ def get_list_of_available_metrics(service_name):
     and merge it into a single list.
     """
 
-    metrics_mapping = get_service_config(service_name)["prometheus_metrics_mapping"]
-    list_of_available_metrics = [v for k, v in metrics_mapping.items()]
-    list_of_available_metrics = list(itertools.chain(*list_of_available_metrics))
+    try:
+        metrics_mapping = get_service_config(service_name)["prometheus_metrics_mapping"]
+        list_of_available_metrics = [v for k, v in metrics_mapping.items()]
+        list_of_available_metrics = list(itertools.chain(*list_of_available_metrics))
+    except Exception as err:
+        LOGGER.error("No available metrics defined in config: %s", err)
 
     return list_of_available_metrics
 
 
 def get_service(service_name):
     """Get service class instance using config"""
-    service = get_service_config(service_name)["service_class"]
-    service_module = get_service_config(service_name)["service_module"]
+    try:
+        service = get_service_config(service_name)["service_class"]
+        service_module = get_service_config(service_name)["service_module"]
 
-    importlib.import_module(service_module)
+        importlib.import_module(service_module)
+    except Exception as err:
+        LOGGER.error("Failed to get service class instance: %s", err)
 
     return getattr(sys.modules[service_module], service)()
 
