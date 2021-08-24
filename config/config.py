526f7854940e959bad25baf4b120f7b196e3bdf6
YoungJun Park <yjpark@homex.com>
2021-08-24 14:08:53 -0400

    chore: remove unused code

--- config/config.py
+++ config/config.py
@@ -10,24 +10,3 @@ class BaseConfig:
     LOG_DATE_FORMAT = "%Y/%m/%d|%H:%M:%S"
     LOG_LOGGING_LEVEL = "INFO"
     DATA_COLLECTION_LOGGER = "DATA-COLLECTION"
-
-class SplunkConfig(BaseConfig):
-    API_URL = "https://api.victorops.com/api-reporting/v2/incidents"
-
-    AVAILABLE_METRICS = [
-        "number_of_incidents",
-        "time_taken_to_acknowledge",
-        "time_taken_to_resolve",
-    ]
-
-    PROMETHEUS_METRICS_MAPPING = {
-        "counter": ["number_of_incidents"],
-        "summary": ["time_taken_to_acknowledge", "time_taken_to_resolve"],
-    }
-
-    dict_of_credentials = get_credentials_from_secret_file(
-        os.environ.get("SPLUNK_API_SECRET_FILE_PATH")
-    )
-
-    API_ID = dict_of_credentials.get("API_ID", "")
-    API_KEY = dict_of_credentials.get("API_KEY", "")
--- src/api_factory.py
+++
@@ -1,9 +0,0 @@
-from src.splunk_api import *
-
-class ApiFactory:
-    @classmethod
-    def get_api_factory(self, service):
-        if service == "splunk":
-            return SplunkApi()
-        else:
-            return
