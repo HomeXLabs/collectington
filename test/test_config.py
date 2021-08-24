e628acf88d8d76c88c837cde6e5360acf23f2b56
Nathan Boswell <nboswell@homex.com>
2021-08-24 16:24:42 +0100

    Add docstrings to test file Reorder imports

--- test/test_config.py
+++ test/test_config.py
@@ -1,10 +1,14 @@
+"""Test that the config parsing module is operating as expected."""
 import unittest
 
-from src.config import parse
 from json.decoder import JSONDecodeError
+from src.config import parse
 
 class TestConfig(unittest.TestCase):
+    """Test that the config parsing module is operating as expected."""
+
     def test_parse_valid_json(self):
+        """Test that function parses json correctly."""
         test_json = '{"json": "test", "one": 1}'
         expected = {
                 "json": "test",
@@ -16,12 +20,14 @@ class TestConfig(unittest.TestCase):
         self.assertEqual(output, expected)
 
     def test_parse_fails_with_invalid_json(self):
+        """Ensure the correct Error is thrown if input isn't valid json."""
         test_string = 'invalid json'
 
         with self.assertRaises(JSONDecodeError):
             parse(test_string)
 
     def test_parse_fails_with_non_string_input(self):
+        """Ensure that a TypeError is thrown if a string isn't provided as an argument."""
         test_input = 3
 
         with self.assertRaises(TypeError):
