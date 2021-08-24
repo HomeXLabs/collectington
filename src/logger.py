70706362c571b0b17ab17baa4d7aa841973af674
YoungJun Park <yjpark@homex.com>
2021-08-24 16:44:52 -0400

    chore: change logging level var name

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,7 @@ import logging
 
 LOG_MSG_FORMAT = "%(asctime)s|%(name)-12s|%(levelname)-8s|%(message)s"
 LOG_DATE_FORMAT = "%Y/%m/%d|%H:%M:%S"
-LOG_LOGGING_LEVEL = "INFO"
+LOGGING_LEVEL = "INFO"
 DATA_COLLECTION_LOGGER = "DATA-COLLECTION"
 
 
@@ -12,7 +12,7 @@ def setup_logging():
     log_formatter = logging.Formatter(LOG_MSG_FORMAT, LOG_DATE_FORMAT)
 
     logger = logging.getLogger(DATA_COLLECTION_LOGGER)
-    logger.setLevel(LOG_LOGGING_LEVEL)
+    logger.setLevel(LOGGING_LEVEL)
 
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(log_formatter)
