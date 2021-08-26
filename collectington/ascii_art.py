d6448f98493ef597c65688abe74cfa7c7b822ae4
Nathan Boswell <nboswell@homex.com>
2021-08-26 10:19:00 +0100

    I'm sick of docstrings

--- collectington/ascii_art.py
+++ collectington/ascii_art.py
@@ -1,8 +1,10 @@
+"""Module for printing ascii introduction."""
 from termcolor import colored
 from pyfiglet import figlet_format
 
 
 def print_ascii():
+    """Function to print ascii introduction."""
     result = colored(figlet_format("Welcome! \n This is Collectington"), color="green")
 
     print(result)
