b913925c18e7a8fc50f6500dfaa4597a1eb381bb
mihai burcea <mburcea@homex.com>
2021-11-19 11:42:48 -0500

    Address review comment: remove unnecessary iteration to find the service name, as it is already available in the current context (thanks to Young).

--- collectington/collectington_api.py
+++ collectington/collectington_api.py
@@ -168,12 +168,12 @@ class CollectingtonApi(ABC):
 
     def _init_p_method(self, p_method, api_metric):
         """Internal method to metric methods with labels only if they're provided."""
-        for service in self.config["services"]:
-            if (self.config["services"][service].get("prometheus_metric_labels") is not None
-                and api_metric in self.config["services"][service]["prometheus_metric_labels"]
-            ):
-                labels = self.config["services"][service]["prometheus_metric_labels"][api_metric]
-                return p_method(api_metric, api_metric, labels)
+        
+        if (self.config["services"][self.service_name].get("prometheus_metric_labels") is not None
+            and api_metric in self.config["services"][self.service_name]["prometheus_metric_labels"]
+        ):
+            labels = self.config["services"][self.service_name]["prometheus_metric_labels"][api_metric]
+            return p_method(api_metric, api_metric, labels)
 
         return p_method(api_metric, api_metric)
 
@@ -205,13 +205,12 @@ class CollectingtonApi(ABC):
         for p_instance in list_of_metric_instances:
             metric = str(p_instance).split(":")[1]
             labels_object = None
-            for service in self.config["services"]:
-                if (
-                    self.config["services"][service].get("prometheus_metric_labels") is not None
-                    and metric in self.config["services"][service]["prometheus_metric_labels"]
-                ):
-                    labels_object = self.config["services"][service]["prometheus_metric_labels"]
-                    break
+            if (
+                self.config["services"][self.service_name].get("prometheus_metric_labels") is not None
+                and metric in self.config["services"][self.service_name]["prometheus_metric_labels"]
+            ):
+                labels_object = self.config["services"][self.service_name]["prometheus_metric_labels"]
+                break
 
             if (labels_object is not None):
                 for labels_and_metric_object in service_metric_dict[metric]:
